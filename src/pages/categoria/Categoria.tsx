fcd297045317ed72c7aeabc7cecd200a03af5b9d
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-12 16:44:21 -0300

    add navbar na Page produtos e categorias

--- src/pages/categoria/Categoria.tsx
+++ src/pages/categoria/Categoria.tsx
@@ -1,8 +1,10 @@
 import ListaCategoria from "../../components/categorias/listaCategoria/ListaCategoria"
+import Navbar from "../../components/navbar/Navbar"
 
 function Categoria() {
   return (
     <>
+    <Navbar />
     <div>
         <h1>Categoria</h1>
     </div>
--- src/pages/produto/Produto.tsx
+++ src/pages/produto/Produto.tsx
@@ -1,8 +1,10 @@
+import Navbar from "../../components/navbar/Navbar"
 import ListaProdutos from "../../components/produtos/listaProdutos/ListaProdutos"
 
 function Produto() {
   return (
     <>
+    <Navbar />
     <div>
         <h1>Produto</h1>
     </div>
