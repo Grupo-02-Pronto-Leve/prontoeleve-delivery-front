d043bcd46cb628e88b430dce15860a3a2f700fed
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-11 21:16:26 -0300

    add Pages

---
+++ src/pages/cadastro/Cadastro.tsx
@@ -0,0 +1,15 @@
+function Cadastro() {
+
+
+    return (
+        <>
+            <div>
+                <h1>
+                    Cadastro
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Cadastro
