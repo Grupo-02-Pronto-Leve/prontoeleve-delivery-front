be3251b24ff9722e36f41ee015a2b17c07c8cd51
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-11 21:37:33 -0300

    add routes and so on

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+// import { AuthProvider } from './contexts/AuthContext'
+
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
+
+import Categoria from './pages/categoria/Categoria'
+import Produto from './pages/produto/Produto'
+import Footer from './components/footer/Footer'
+
+
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
   return (
     <>
-      <div>
-          <h1>PRONTO & LEVE</h1>
-      </div>
+      {/* <AuthProvider> */}
+        <ToastContainer />
+        <BrowserRouter>
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/categorias" element={<Categoria />} />
+              <Route path="/produtos" element={<Produto />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      {/* </AuthProvider> */}
     </>
   )
 }
